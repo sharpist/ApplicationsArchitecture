bb7db112f75bd2d5c15640fcfc680743274a6241
Alexander Usov <webapp@programmer.net>
2022-02-01 22:27:20 +0300

    lesson #2

--- CleanArchitecture/CleanArchitecture.Core/Middlewares/ExceptionMiddleware.cs
+++ CleanArchitecture/CleanArchitecture.Core/Middlewares/ExceptionMiddleware.cs
@@ -9,23 +9,23 @@ public static class ExceptionMiddleware
             appError.Run(async context =>
             {
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                context.Response.ContentType = "application/json";
+                context.Response.ContentType = "application/problem+json";
 
                 var contextFeature = context.Features.Get<IExceptionHandlerPathFeature>();
                 if (contextFeature is not null)
                 {
-                    var ex = contextFeature?.Error;
+                    var ex = contextFeature.Error;
                     var isDev = env.IsDevelopment();
 
                     await context.Response.WriteAsync(JsonSerializer.Serialize<ProblemDetails>(
                         new ProblemDetails
                         {
-                            Type = ex!.GetType().Name,
+                            Type = ex.GetType().Name,
                             Status = (int)HttpStatusCode.InternalServerError,
-                            Instance = contextFeature?.Path,
+                            Instance = contextFeature.Path,
                             Title = isDev ? $"{ex.Message}" : "An error occurred.",
                             Detail = isDev ? ex.StackTrace : null
-                        }));
+                        })).ConfigureAwait(false);
                 }
             });
         });
